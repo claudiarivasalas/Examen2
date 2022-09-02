3bf8aeec03e19acc8df6082e7bcfb60dd1ca4f95
claudiarivasalas <claudia.rivas.salas@gmail.com>
2022-09-02 11:37:32 -0400

    Examen2

---
+++ script.js
@@ -0,0 +1,9 @@
+function cambiar(element) {
+    element.innerText = "Not Avaulable";
+}
+function eliminar(element){
+    element.remove();
+}
+function mensaje_alerta(element){
+    alert("Pagina en Construccion");
+}
